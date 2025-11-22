75e5979ed14d6b0f375dc6f0bd3433019c62831e
DDANGE <royalkdy@gmail.com>
2025-11-22 22:42:22 +0900

    login2 - guard&cookie를 사용한 로그인 인증

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Body, Post, Request, Response } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Request,
+  Response,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/user/user.dto';
 import { AuthService } from './auth.service';
+import { LoginGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -26,5 +35,25 @@ export class AuthController {
       return res.json({ message: '로그인 성공' });
     }
     return res.json({ message: '로그인 실패' });
+  }
+
+  @UseGuards(LoginGuard)
+  @Post('login2')
+  async login2(@Request() req, @Response() res) {
+    if (!req.cookies['login'] && req.user) {
+      res.cookie('login', JSON.stringify(req.user), {
+        httpOnly: true,
+        maxAge: 1000 * 10,
+      });
+    }
+    console.log(req.user);
+    console.log(res.cookie['login']);
+    return res.send({ message: 'login2 success' });
+  }
+
+  @UseGuards(LoginGuard)
+  @Get('test-guard')
+  testGuard() {
+    return '로그인이 된때에만 글이 보입니다.';
   }
 }
