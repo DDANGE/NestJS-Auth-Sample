eafe353d9704c566cbdd9290b387cc50ef2ea3b0
DDANGE <royalkdy@gmail.com>
2025-11-22 21:28:33 +0900

    login api 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Get, Post } from '@nestjs/common';
+import { Controller, Body, Post, Request, Response } from '@nestjs/common';
 import { CreateUserDto } from 'src/user/user.dto';
 import { AuthService } from './auth.service';
 
@@ -9,5 +9,22 @@ export class AuthController {
   @Post('register')
   async register(@Body() userDto: CreateUserDto) {
     return await this.authService.register(userDto);
+  }
+
+  @Post('login')
+  async login(@Request() req, @Response() res) {
+    const userInfo = await this.authService.validateUser(
+      req.body.email,
+      req.body.password,
+    );
+
+    if (userInfo) {
+      res.cookie('login', JSON.stringify(userInfo), {
+        httpOnly: false,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      });
+      return res.json({ message: '로그인 성공' });
+    }
+    return res.json({ message: '로그인 실패' });
   }
 }
